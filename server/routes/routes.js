680c9a87953c6c5bf0fed2ba600d7cb055022d52
Rasheed M <r.muslimani@gmail.com>
2018-09-25 14:18:22 -0400

    Initial Commit with client-folder and server-folder separating FE from BE

---
+++ server/routes/routes.js
@@ -0,0 +1,28 @@
+
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var Expense = require('../../models/Expense');
+
+router.get('/', function(req, res){
+  res.render('index')
+});
+
+router.route('/insert')
+.post(function(req, res) {
+	var expense = new Expense();
+	expense.description = req.body.desc;
+	expense.amount = req.body.amount;
+	expense.month = req.body.month;
+	expense.year = req.body.year;
+	
+expense.save(function(err) {
+	if (err) {
+		res.send(err);
+	}
+	res.send('Expense successfully added!');
+});
+});
+
+
+module.exports = router;
