f3d0a636dc49802bf31eee022c5b8d6e28896ca1
Rasheed M <r.muslimani@gmail.com>
2018-09-25 15:57:20 -0400

    Add MongoDB database and connect to back-end

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -24,5 +24,53 @@ expense.save(function(err) {
 });
 });
 
+router.route('/update')
+.post(function(req, res) {
+	const doc = {
+		description: req.body.description,
+		amount: req.body.amount,
+		month: req.body.month,
+		year: req.body.year
+	};
+	console.log(doc);
+	//WHY capital EXPENSE??
+	Expense.update({_id: req.body._id}, doc, function(err, result) {
+		if (err) {
+			res.send(err);
+		}
+		res.send('Expense successfully updated!');
+	});
+});
+
+router.get('/delete', function(req, res) {
+	var id = req.query.id;
+	Expense.find({_id: id}).remove().exec(function(err, expense) {
+		if(err) {
+			res.send(err);
+		}
+		res.send('Expense successfully deleted!');
+	});
+});
+
+router.get('/getAll', function(req, res) {
+	var monthRec = req.query.month;
+	var yearRec = req.query.year;
+	
+	if(monthRec && monthRec != 'All') {
+		Expense.find({$and: [ {month: monthRec}, {year: yearRec}]}, function(err, expenses) {
+			if(err) {
+				res.send(err);
+			}
+			res.json(expenses);
+		});
+	} else {
+		Expense.find({year: yearRec}, function(err, expenses) {
+			if(err) {
+				res.send(err);
+			}
+			res.json(expenses);
+		});
+	}
+});
 
 module.exports = router;
